e8c4b8b4f6f4f743a64eb22092be4902e1ad6b9b
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-06-22 20:23:24 -0500

    Updating Movie model to make Release Date more user friendly

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,8 @@ namespace MvcMovie.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
