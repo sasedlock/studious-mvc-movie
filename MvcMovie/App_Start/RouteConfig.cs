695c9fe5e0e107ea510dbd27c72f69663fff3b43
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-06-22 20:12:13 -0500

    Adding seed method, and link to movies controller in shared layout

--- MvcMovie/App_Start/RouteConfig.cs
+++ MvcMovie/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using MvcMovie.Models;
 
 namespace MvcMovie
 {
@@ -18,6 +19,8 @@ namespace MvcMovie
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            SeedData.Initialize();
         }
     }
 }
