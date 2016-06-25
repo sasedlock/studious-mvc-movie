228f3d7cf7630a01d9445dc81150b17c14f8f0ee
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-06-25 18:19:59 -0500

    Adding validation to Movie model

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -9,12 +9,20 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3,ErrorMessage = "Movie title must be between 3 and 60 characters in length")]
         public string Title { get; set; }
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-z13\-\s]*$")]
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 }
