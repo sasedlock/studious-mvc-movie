b87cec44cfcb5e5066eca8bc69bb9844fd17dbeb
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-06-29 20:17:24 -0500

    Implementing stubbed out MovieService methods

--- MvcMovie.Mvc/Controllers/MoviesController.cs
+++ MvcMovie.Mvc/Controllers/MoviesController.cs
@@ -15,32 +15,16 @@ namespace MvcMovie.Controllers
 {
     public class MoviesController : Controller
     {
-        private MvcMovieDbContext db = new MvcMovieDbContext();
         private MovieService _service = new MovieService();
 
         // GET: Movies
         public ActionResult Index(string movieGenre, string name)
         {
-            //var genres = from m in db.Movies
-            //             orderby m.Genre
-            //             select m.Genre;
             var genres = _service.GetGenres();
 
             ViewBag.movieGenre = new SelectList(genres.Distinct().ToList());
-
-            //var movies = from m in db.Movies
-            //             select m;
+            
             var movies = _service.GetMovies(movieGenre, name);
-
-            //if (!string.IsNullOrEmpty(name))
-            //{
-            //    movies = movies.Where(s => s.Title.Contains(name));
-            //}
-
-            //if (!string.IsNullOrEmpty(movieGenre))
-            //{
-            //    movies = movies.Where(m => m.Genre == movieGenre);
-            //}
 
             return View(movies.ToList());
         }
@@ -52,8 +36,7 @@ namespace MvcMovie.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            //Movie movie = db.Movies.Find(id);
-            var movie = _service.GetMovieById(id);
+            var movie = _service.GetMovieById((int) id);
             if (movie == null)
             {
                 return HttpNotFound();
@@ -76,8 +59,6 @@ namespace MvcMovie.Controllers
         {
             if (ModelState.IsValid)
             {
-                //    db.Movies.Add(movie);
-                //    db.SaveChanges();
                 _service.AddMovie(movie);
                 return RedirectToAction("Index");
             }
@@ -92,8 +73,7 @@ namespace MvcMovie.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            //Movie movie = db.Movies.Find(id);
-            var movie = _service.GetMovieById(id);
+            var movie = _service.GetMovieById((int) id);
             if (movie == null)
             {
                 return HttpNotFound();
@@ -110,8 +90,6 @@ namespace MvcMovie.Controllers
         {
             if (ModelState.IsValid)
             {
-                //db.Entry(movie).State = EntityState.Modified;
-                //db.SaveChanges();
                 _service.EditMovie(movie);
                 return RedirectToAction("Index");
             }
@@ -125,8 +103,7 @@ namespace MvcMovie.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            //Movie movie = db.Movies.Find(id);
-            var movie = _service.GetMovieById(id);
+            var movie = _service.GetMovieById((int) id);
             if (movie == null)
             {
                 return HttpNotFound();
@@ -139,20 +116,8 @@ namespace MvcMovie.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            //Movie movie = db.Movies.Find(id);
-            //db.Movies.Remove(movie);
-            //db.SaveChanges();
             _service.DeleteMovie(id);
             return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
--- MvcMovie.Service/Services/MovieService.cs
+++ MvcMovie.Service/Services/MovieService.cs
@@ -1,42 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MvcMovie.DataAccess.Context;
 using MvcMovie.Domain.Models;
 
 namespace MvcMovie.Service.Services
 {
     public class MovieService
     {
+        private MvcMovieDbContext db = new MvcMovieDbContext(); 
+
         public IEnumerable<String> GetGenres()
         {
-            throw new NotImplementedException();
+            var genres = from m in db.Movies
+                         orderby m.Genre
+                         select m.Genre;
+
+            return genres;
         }
 
         public IEnumerable<Movie> GetMovies(string movieGenre, string name)
         {
-            throw new NotImplementedException();
+            var movies = from m in db.Movies
+                         select m;
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                movies = movies.Where(s => s.Title.Contains(name));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(m => m.Genre == movieGenre);
+            }
+
+            return movies;
         }
 
-        public Movie GetMovieById(int? id)
+        public Movie GetMovieById(int id)
         {
-            throw new NotImplementedException();
+            return db.Movies.Find(id);
         }
 
         public void AddMovie(Movie movie)
         {
-            throw new NotImplementedException();
+            db.Movies.Add(movie);
+            db.SaveChanges();
         }
 
         public void EditMovie(Movie movie)
         {
-            throw new NotImplementedException();
+            db.Entry(movie).State = EntityState.Modified;
+            db.SaveChanges();
         }
 
         public void DeleteMovie(int id)
         {
-            throw new NotImplementedException();
+            var movie = db.Movies.Find(id);
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
         }
     }
 }
