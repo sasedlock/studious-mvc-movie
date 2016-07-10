bcd63d6685dfe5e364e3baf7d427903fba79bad5
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-07-10 18:58:31 -0500

    Consolidating DI registration to its own static method

---
+++ MvcMovie.Mvc/App_Start/DiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using MvcMovie.Service.Interfaces;
+using MvcMovie.Service.Services;
+
+namespace MvcMovie
+{
+    public class DiConfig
+    {
+        public static void RegisterDependencyInjection()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<MovieService>().As<IMovieService>().InstancePerRequest();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
--- MvcMovie.Mvc/Global.asax.cs
+++ MvcMovie.Mvc/Global.asax.cs
@@ -5,10 +5,6 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Autofac;
-using Autofac.Integration.Mvc;
-using MvcMovie.Service.Interfaces;
-using MvcMovie.Service.Services;
 
 namespace MvcMovie
 {
@@ -20,14 +16,7 @@ namespace MvcMovie
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            
-            var builder = new ContainerBuilder();
-
-            builder.RegisterControllers(typeof (MvcApplication).Assembly);
-            builder.RegisterType<MovieService>().As<IMovieService>().InstancePerRequest();
-
-            var container = builder.Build();
-            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            DiConfig.RegisterDependencyInjection();
         }
     }
 }
