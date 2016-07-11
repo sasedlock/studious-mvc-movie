d9df8c672a74a4ab8220a7b8b8bc50d9d6d9d132
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-07-10 19:45:56 -0500

    Encapsulating DI registration for Movie into an AutoFac module

--- MvcMovie.Mvc/App_Start/DiConfig.cs
+++ MvcMovie.Mvc/App_Start/DiConfig.cs
@@ -4,6 +4,7 @@ using Autofac.Integration.Mvc;
 using MvcMovie.DataAccess.Context;
 using MvcMovie.DataAccess.Dals;
 using MvcMovie.DataAccess.Interfaces;
+using MvcMovie.Modules;
 using MvcMovie.Service.Interfaces;
 using MvcMovie.Service.Services;
 
@@ -16,9 +17,8 @@ namespace MvcMovie
             var builder = new ContainerBuilder();
 
             builder.RegisterControllers(typeof(MvcApplication).Assembly);
-            builder.RegisterType<MovieService>().As<IMovieService>().InstancePerRequest();
-            builder.RegisterType<MovieDal>().As<IMovieDal>().InstancePerRequest();
-            builder.RegisterType<MvcMovieDbContext>().As<IMvcMovieDbContext>().InstancePerRequest();
+
+            builder.RegisterModule(new MovieModule());
 
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
---
+++ MvcMovie.Mvc/Modules/MovieModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+using MvcMovie.DataAccess.Context;
+using MvcMovie.DataAccess.Dals;
+using MvcMovie.DataAccess.Interfaces;
+using MvcMovie.Service.Interfaces;
+using MvcMovie.Service.Services;
+
+namespace MvcMovie.Modules
+{
+    public class MovieModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<MovieService>().As<IMovieService>().InstancePerRequest();
+            builder.RegisterType<MovieDal>().As<IMovieDal>().InstancePerRequest();
+            builder.RegisterType<MvcMovieDbContext>().As<IMvcMovieDbContext>().InstancePerRequest();
+        }
+    }
+}
