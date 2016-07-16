244a33f375a3b0d55f47850a04f946158d140bdf
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-07-16 16:31:42 -0500

    Adding DataAccessTests project, test folder and initial test class, as well as installing Moq

---
+++ MvcMovie.DataAccessTests/Dals/MovieDalTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MvcMovie.DataAccessTests.Dals
+{
+    [TestClass]
+    public class MovieDalTests
+    {
+
+
+        [ClassInitialize]
+        public void InitializeMovieDalTestsClass()
+        {
+        }
+
+        [TestMethod]
+        public void GetGenres_WithResults_ReturnsResults()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+
+        }
+    }
+}
