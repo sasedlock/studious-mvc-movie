0d7557e9d56dd2db34cb7eea5858eb0985debe96
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-07-16 16:37:57 -0500

    Adding reference to Moq in correct project, and adding initialization code

--- MvcMovie.DataAccessTests/Dals/MovieDalTests.cs
+++ MvcMovie.DataAccessTests/Dals/MovieDalTests.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcMovie.DataAccess.Dals;
+using MvcMovie.DataAccess.Interfaces;
+using Moq;
 
 namespace MvcMovie.DataAccessTests.Dals
 {
     [TestClass]
     public class MovieDalTests
     {
+        private Mock<IMvcMovieDbContext> _mockDbContext;
+        private MovieDal _dal;
 
 
         [ClassInitialize]
         public void InitializeMovieDalTestsClass()
         {
+            _mockDbContext = new Mock<IMvcMovieDbContext>();
+            _dal = new MovieDal(_mockDbContext.Object);
         }
 
         [TestMethod]
