6a44a5c3da382e3e8a242565d6364284cc4523c7
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-08-13 12:32:33 -0500

    Adding initial integration test

---
+++ MvcMovie.IntegrationTests/MovieIntegration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcMovie.Controllers;
+using MvcMovie.DataAccess.Dals;
+using MvcMovie.Service.Services;
+
+namespace MvcMovie.IntegrationTests
+{
+    [TestClass]
+    public class MovieIntegration
+    {
+        private MoviesController _controller;
+        private MovieService _service;
+        private MovieDal _dal;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            
+        }
+    }
+}
