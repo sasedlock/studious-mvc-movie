9177a4a69f7203c09eca068fb4ead32482a9d693
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-09-11 15:34:01 -0500

    Adding service layer class and methods

---
+++ Service/MovieService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using Domain;
+
+namespace Service
+{
+    public class MovieService
+    {
+        private MovieDal _dal = new MovieDal();
+
+        #region Queries
+
+        public List<Movie> GetMovies()
+        {
+            return _dal.GetMovies().ToList();
+        }
+
+        public List<string> GetGenres()
+        {
+            return _dal.GetGenres().ToList();
+        }
+
+        public Movie GetMovieById(int id)
+        {
+            return _dal.GetMovieById(id);
+        }
+
+        public List<Movie> GetMoviesByNameAndGenre(string name, string genre)
+        {
+            var movies = _dal.GetMovies();
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                movies = movies.Where(m => m.Title.Contains(name));
+            }
+
+            if (!string.IsNullOrEmpty(genre))
+            {
+                movies = movies.Where(m => m.Genre == genre);
+            }
+
+            return movies.ToList();
+        }
+
+        #endregion
+
+        #region Commands
+
+        public void AddMovie(Movie movie)
+        {
+            if (MovieIsValid(movie))
+            {
+                _dal.AddMovie(movie);
+            }
+        }
+
+        public void EditMovie(Movie movie)
+        {
+            if (MovieIsValid(movie))
+            {
+                _dal.EditMovie(movie);
+            }
+        }
+
+        public void DeleteMovie(int id)
+        {
+            if (_dal.GetMovieById(id) != null)
+            {
+                _dal.DeleteMovie(id);
+            }
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private bool MovieIsValid(Movie movie)
+        {
+            return !string.IsNullOrEmpty(movie.Genre) 
+                && !string.IsNullOrEmpty(movie.Rating) 
+                && !string.IsNullOrEmpty(movie.Title);
+        }
+
+        #endregion
+    }
+}
